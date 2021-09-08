ce1bd7b0a2cbb5e032fca308711e01be7749a355
alanaman <40354271+alanaman@users.noreply.github.com>
2021-09-08 18:22:47 +0530

    added firtname(mandatory) and lastname fields

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 
-from django.contrib.auth.forms import UserCreationForm
+from accounts.forms import RegistrationForm
 from django.urls import reverse_lazy
 from django.views import generic
 
 
 class SignUpView(generic.CreateView):
-    form_class = UserCreationForm
+    form_class = RegistrationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
