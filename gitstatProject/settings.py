b341db6bdd385273ff11587fe48d8eb792f981b6
alanaman <40354271+alanaman@users.noreply.github.com>
2021-09-08 16:25:45 +0530

    switching to postgresql, adding signup

--- gitstatProject/settings.py
+++ gitstatProject/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'gitstatProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'gitstatDB',
+        'USER': 'postgres',
+        'PASSWORD': '112114',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
